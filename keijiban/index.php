17931988a724c6d9c9b981db518690a7e1d22b9d
irohas244 <38972312+irohas244@users.noreply.github.com>
2018-07-11 20:54:31 +0900

    Update index.php

--- keijiban/index.php
+++ keijiban/index.php
@@ -21,7 +21,18 @@
         <img src="4eachblog_logo.jpg">
          <header>
         <li>
-            <ul> 
+            <ul>トップ</ul>
+            <ul>プロフィール</ul>
+            <ul>4eachについて</ul>
+            <ul>登録フォーム</ul>
+            <ul>問い合わせ</ul>
+            <ul><その他/ul>
+                </li>
+                
+                </header>
+            <h2>プログラミングに役立つ掲示板</h2>
+            
+            
     
     </body>
 </html>
